a792fffcab8a76257667514d9155e20972c42fe4
森井ゴンザレス <morygonzalez@gmail.com>
2011-06-16 01:20:28 +0900

    Markdown使えるようになった

--- public/plugin/lokka-markdown/lib/lokka/markdown.rb
+++ public/plugin/lokka-markdown/lib/lokka/markdown.rb
@@ -1,14 +1,6 @@
 require 'kramdown'
 
 module Lokka
-  module Markdown
-    def self.registered(app)
-      app.before do
-        @post = Post.new
-      end
-    end
-  end
-
   module Helpers
     def markdown(str=nil)
       Kramdown::Document.new(str).to_html
