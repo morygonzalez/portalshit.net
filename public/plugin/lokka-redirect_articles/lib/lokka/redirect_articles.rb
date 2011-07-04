ce6989df1081b563df0e3e871f5540d5b95b84f9
森井ゴンザレス <morygonzalez@gmail.com>
2011-07-05 00:19:01 +0900

    redirect plugins

---
+++ public/plugin/lokka-redirect_articles/lib/lokka/redirect_articles.rb
@@ -0,0 +1,12 @@
+module Lokka
+  module RedirectArticles
+    def self.registered(app)
+      app.get %r{/article\.php} do
+        unless request.query_string.empty?
+          /id=(\d+)?/ =~ request.query_string
+          redirect "/#{$1}", 301
+        end
+      end
+    end
+  end
+end
