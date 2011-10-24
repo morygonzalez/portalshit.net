ccbb65d4a4bacc0f048714f0ab7898c3529449c3
森井ゴンザレス <morygonzalez@gmail.com>
2011-10-24 22:20:22 +0900

    commented out `require "rack/pygments"`

--- public/plugin/lokka-markdown/lib/lokka/markdown.rb
+++ public/plugin/lokka-markdown/lib/lokka/markdown.rb
@@ -1,7 +1,4 @@
 require 'kramdown'
-require "rack/pygments"
-
-Rack::Pygments.new(:html_tag => "highlight", :html_attr => "lang")
 
 module Lokka
   module Markdown
@@ -11,16 +8,10 @@ module Lokka
     def markdown(str=nil)
       html = Kramdown::Document.new(str).to_html
       html = prettify(html)
-      # pygmentize(html) 
     end
 
     def prettify(html)
       html.gsub!(/<pre>/, "<pre class=\"prettyprint\">")
-      return html
-    end
-
-    def pygmentize(html)
-      html.gsub!(/<pre>(.+?)<\/pre>/) { "<highlight>#{$1}</highlight>" }
       return html
     end
   end
