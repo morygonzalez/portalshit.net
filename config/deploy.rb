c9947daae0c966cf8f645eba0d475eb0d7da8f61
森井ゴンザレス <morygonzalez@gmail.com>
2011-12-24 17:11:33 +0900

    capistranize中

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "portal shit!"
+set :repository,  "https://github.com/morygonzalez/lokka.git"
+set :branch, "portalshit"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "morygonzalez"
+set :use_sudo, false
+
+role :web, "49.212.0.51"                          # Your HTTP server, Apache/etc
+role :app, "49.212.0.51"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+set :deploy_to, "/home/morygonzalez/sites/www.portalshit.net"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -20,3 +26,17 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+namespace :deploy do
+  task :start do
+    run "unicorn -c unicorn.rb -D -E production"
+  end
+
+  task :stop do
+    run "kill `cat tmp/unicorn-lokka.pid`"
+  end
+
+  task :restart, :role => :app do
+    run "kill -USR2 `cat tmp/unicorn-lokka.pid`"
+  end
+end
