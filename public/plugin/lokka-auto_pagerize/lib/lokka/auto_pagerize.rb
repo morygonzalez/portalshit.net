446d312de82437662966a3238e3345c12f3b11fc
森井ゴンザレス <morygonzalez@gmail.com>
2012-04-15 23:07:49 +0900

    make lokka autopagerizable

---
+++ public/plugin/lokka-auto_pagerize/lib/lokka/auto_pagerize.rb
@@ -0,0 +1,22 @@
+module Lokka
+  module AutoPagerize
+    def self.registered(app); end
+  end
+end
+
+module DataMapper
+  class Pager
+    private
+      def link_to(page, contents=nil, rel={})
+        %(<a href="#{uri_for(page)}" rel="#{rel[:rel]}">#{contents || page}</a>)
+      end
+
+      def previous_link
+        li 'previous jump', link_to(previous_page, option(:previous_text), {:rel => "prev"}) if previous_page
+      end
+
+      def next_link
+        li 'next jump', link_to(next_page, option(:next_text), {:rel => "next"}) if next_page
+      end
+  end
+end
