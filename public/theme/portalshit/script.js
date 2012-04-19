e59ce56d57e287e4fe192b8e5f98b012175e533f
森井ゴンザレス <morygonzalez@gmail.com>
2012-04-19 23:04:36 +0900

    supports AutoPagerize_DOMNodeInserted

--- public/theme/portalshit/script.js
+++ public/theme/portalshit/script.js
@@ -1,3 +1,15 @@
-$(function() {
-  $("pre").attr("class", "prettyprint");
-});
+(function() {
+  var init = function(node) {
+    $(node).find("pre").attr("class", "prettyprint");
+    prettyPrint();
+  }
+
+  document.body.addEventListener('AutoPagerize_DOMNodeInserted',function(evt){
+    var node = evt.target;
+    var requestURL = evt.newValue;
+    var parentNode = evt.relatedNode;
+    init(node);
+  }, false);
+
+  init(document);
+})();
