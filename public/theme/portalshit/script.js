a0029a25102ad4e51f401def59d6d50dbb1fd3ed
森井ゴンザレス <morygonzalez@gmail.com>
2012-09-16 18:03:14 +0900

    Stop using google-code-prettify

--- public/theme/portalshit/script.js
+++ public/theme/portalshit/script.js
@@ -1,7 +1,48 @@
 (function() {
   var init = function(node) {
-    $(node).find("pre").attr("class", "prettyprint");
-    prettyPrint();
+    $(node).find(".body > pre").each(function() {
+      var self = this;
+      $.ajax({
+        type: 'POST',
+        url: '/pygmentize',
+        data: { snippet: $(self).text(), lexar: '' }
+      }).done(function(data) {
+        $(self).replaceWith(data);
+      });
+    });
+
+    /*
+    $('.body').each(function() {
+
+      var Pygmentize = function(lexar, snippet) {
+        var result;
+
+        $.ajax({
+          type: 'POST',
+          url: '/pygmentize',
+          async: false,
+          data: {
+            lexar: lexar,
+            snippet: snippet
+          },
+        }).done(function(data) {
+          result = data;
+        });
+
+        return result;
+      }
+
+      var entryBody = $(this).text();
+
+      entryBody = entryBody.replace(/```(.+?)\n([\s\S]*?)```/g,
+        function(whole, lexar, snippet) {
+          return Pygmentize(lexar, snippet);
+        }
+      );
+
+      $(this).html(entryBody);
+    })
+    */
   }
 
   document.body.addEventListener('AutoPagerize_DOMNodeInserted',function(evt){
