4f6f610c74295157601186128f23859478bd8639
森井ゴンザレス <morygonzalez@gmail.com>
2013-01-15 13:58:07 +0900

    Archive feature

--- public/plugin/lokka-archives/lib/lokka/archives.rb
+++ public/plugin/lokka-archives/lib/lokka/archives.rb
@@ -2,10 +2,22 @@ module Lokka
   module Archives
     def self.registered(app)
       app.get '/archives' do
-        @posts = Post.all
+        @month_posts = posts_group_by_month
         @bread_crumbs = [{:name => t('home'), :link => '/'}]
         @bread_crumbs << {:name => t('archives'), :link => '/archives'}
         haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
+      end
+    end
+  end
+
+  module Helpers
+    def posts_group_by_month
+      posts_all = Post.all
+      months.each_with_object({}) do |month, posts|
+        posts_select_by_month = posts_all.select {|post|
+          post.created_at.strftime('%Y-%m') == "#{month.year}-#{month.month}"
+        }
+        posts[month] = posts_select_by_month
       end
     end
   end
