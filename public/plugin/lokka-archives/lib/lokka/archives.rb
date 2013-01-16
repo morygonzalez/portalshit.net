c56cc87e3a92016126fcd6712ece43caf09ac30f
森井ゴンザレス <morygonzalez@gmail.com>
2013-01-17 08:33:18 +0900

    search by year

--- public/plugin/lokka-archives/lib/lokka/archives.rb
+++ public/plugin/lokka-archives/lib/lokka/archives.rb
@@ -1,8 +1,8 @@
 module Lokka
   module Archives
     def self.registered(app)
-      app.get '/archives' do
-        @month_posts = posts_group_by_month
+      app.get %r{/archives/?(\d{4})} do |year|
+        @month_posts = posts_group_by_month(year)
         @bread_crumbs = [{:name => t('home'), :link => '/'}]
         @bread_crumbs << {:name => t('archives'), :link => '/archives'}
         haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
@@ -11,13 +11,20 @@ module Lokka
   end
 
   module Helpers
-    def posts_group_by_month
-      posts_all = Post.all
+    def posts_group_by_month(target_year = Date.today.year)
+      first_post_year = Post.all.first.created_at.to_date.year
+      last_post_year = Post.all.last.created_at.to_date.year
+      posts_all = Post.all(
+        :created_at.gte => Time.parse("#{target_year}-01-01"),
+        :created_at.lte => Time.parse("#{target_year}-12-31")
+      )
       months.each_with_object({}) do |month, posts|
-        posts_select_by_month = posts_all.select {|post|
-          post.created_at.strftime('%Y-%m') == "#{month.year}-#{month.month}"
-        }
-        posts[month] = posts_select_by_month
+        if month.year == target_year
+          posts_select_by_month = posts_all.select {|post|
+            post.created_at.strftime('%Y-%m') == "#{month.year}-#{month.month}"
+          }
+          posts[month] = posts_select_by_month
+        end
       end
     end
   end
