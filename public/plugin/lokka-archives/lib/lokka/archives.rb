93f7f325085878e3ab97add44843d6371cb5a6b3
森井ゴンザレス <morygonzalez@gmail.com>
2013-01-26 16:45:47 +0900

    Update view

--- public/plugin/lokka-archives/lib/lokka/archives.rb
+++ public/plugin/lokka-archives/lib/lokka/archives.rb
@@ -7,16 +7,24 @@ module Lokka
         @bread_crumbs << {:name => t('archives'), :link => '/archives'}
         haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
       end
+
+      app.before do
+        assets_path = "/plugin/lokka-archives/assets"
+        content_for :header do
+          text = <<-EOS
+          <link href="#{assets_path}/style.css" rel="stylesheet" type="text/css" />
+          EOS
+        end
+      end
     end
   end
 
   module Helpers
     def posts_group_by_month(target_year = Date.today.year)
       first_post_year = Post.all.first.created_at.to_date.year
-      last_post_year = Post.all.last.created_at.to_date.year
+      last_post_year  = Post.all.last.created_at.to_date.year
       posts_all = Post.all(
-        :created_at.gte => Time.parse("#{target_year}-01-01"),
-        :created_at.lte => Time.parse("#{target_year}-12-31")
+        :created_at => (Time.parse("#{target_year}-01-01")..Time.parse("#{target_year}-12-31"))
       )
       months.each_with_object({}) do |month, posts|
         if month.year == target_year
