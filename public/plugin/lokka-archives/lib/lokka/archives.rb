37ef90e2a593af8a0dc08c084b3b3e925b784384
森井ゴンザレス <morygonzalez@gmail.com>
2013-01-29 22:44:25 +0900

    separate index and show action

--- public/plugin/lokka-archives/lib/lokka/archives.rb
+++ public/plugin/lokka-archives/lib/lokka/archives.rb
@@ -1,18 +1,32 @@
 module Lokka
   module Archives
     def self.registered(app)
-      app.get %r{/archives/?(\d{4})?} do |year|
-        @month_posts = posts_group_by_month(year)
+      app.get '/archives' do
+        posts = Post.all(:created_at => (1.year.ago..Time.now))
+        @month_posts = posts_group_by_month(posts)
         @bread_crumbs = [{:name => t('home'), :link => '/'}]
         @bread_crumbs << {:name => t('archives'), :link => '/archives'}
+        haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
+      end
+
+      app.get '/archives/:year' do |year|
+        posts = Post.all(
+          :created_at => (
+            Time.parse("#{year}-01-01T00:00:00")..Time.parse("#{year}-12-31T23:59:59")
+          )
+        )
+        @month_posts = posts_group_by_month(posts)
+        @bread_crumbs = [{:name => t('home'), :link => '/'}]
+        @bread_crumbs << {:name => t('archives'), :link => '/archives'}
+        @bread_crumbs << {:name => year, :link => "/archives/#{year}"}
         haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
       end
 
       app.before do
         assets_path = "/plugin/lokka-archives/assets"
         content_for :header do
-          text = <<-EOS
-          <link href="#{assets_path}/style.css" rel="stylesheet" type="text/css" />
+          text = <<-EOS.strip_heredoc
+            <link href="#{assets_path}/style.css" rel="stylesheet" type="text/css" />
           EOS
         end
       end
@@ -20,20 +34,13 @@ module Lokka
   end
 
   module Helpers
-    def posts_group_by_month(target_year = Date.today.year)
-      first_post_year = Post.all.first.created_at.to_date.year
-      last_post_year  = Post.all.last.created_at.to_date.year
-      posts_all = Post.all(
-        :created_at => (Time.parse("#{target_year}-01-01")..Time.parse("#{target_year}-12-31"))
-      )
-      months.each_with_object({}) do |month, posts|
-        if month.year == target_year
-          posts_select_by_month = posts_all.select {|post|
-            post.created_at.strftime('%Y-%m') == "#{month.year}-#{month.month}"
-          }
-          posts[month] = posts_select_by_month
-        end
+    def posts_group_by_month(posts)
+      result = months.each_with_object({}) do |month, result|
+        result[month] = posts.select {|post|
+          post.created_at.strftime('%Y-%m') == "#{month.year}-#{month.month}"
+        }
       end
+      result.delete_if {|key, value| value.blank? }
     end
 
     def year_list
