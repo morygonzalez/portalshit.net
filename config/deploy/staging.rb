7095a4b58abb127cd9f02ef06535a8cb5876f7e4
森井ゴンザレス <morygonzalez@gmail.com>
2013-04-13 11:07:26 +0900

    use staging server's mysql

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 puts "*** Deploying to the \033[1;42m  STAGING  \033[0m server!"
 
 set :domain, '219.94.232.157'
-set :db_host, '54.248.96.173'
+set :db_host, domain
 role :web, domain
 role :app, domain
 role :db,  domain
