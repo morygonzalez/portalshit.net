5c1a63389727d3cc2635407ed334849182c8c199
森井ゴンザレス <morygonzalez@gmail.com>
2013-04-27 15:56:47 +0900

    Add task to create newrelic symlink

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,3 +5,11 @@ set :db_host, '219.94.232.157'
 role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
+
+namespace :deploy do
+  desc "Create newrelic symlink"
+  task :newrelic_symlink, :role => :app, :except => { :no_release => true } do
+    run "ln -sfn #{shared_path}/newrelic.yml #{current_path}/config/newrelic.yml"
+  end
+  before [:"deploy:start", :"deploy:restart"], :"deploy:newrelic_symlink"
+end
