b7138f62f39d9b320a76b32021aedb3771eb784b
森井ゴンザレス <morygonzalez@gmail.com>
2013-06-02 10:41:47 +0900

    Make controller slim and add i18n for archive page

--- public/plugin/lokka-archives/lib/lokka/archives.rb
+++ public/plugin/lokka-archives/lib/lokka/archives.rb
@@ -4,20 +4,18 @@ module Lokka
       app.set :cache_enabled, true
 
       app.get '/archives' do
-        posts = Post.all(:created_at => (1.year.ago..Time.now))
-        @month_posts = posts_group_by_month(posts)
+        @month_posts = Post.all(:created_at => (1.year.ago..Time.now)).
+          group_by {|post| post.created_at.beginning_of_month }
         @bread_crumbs = [{:name => t('home'), :link => '/'}]
         @bread_crumbs << {:name => t('archives.title'), :link => '/archives'}
         haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
       end
 
       app.get '/archives/:year' do |year|
-        posts = Post.all(
+        @month_posts = Post.all(
           :created_at => (
-            Time.parse("#{year}-01-01T00:00:00")..Time.parse("#{year}-12-31T23:59:59")
-          )
-        )
-        @month_posts = posts_group_by_month(posts)
+            Time.parse("#{year}-01-01T00:00:00")..Time.parse("#{year}-12-31T23:59:59"))).
+          group_by {|post| post.created_at.beginning_of_month }
         @bread_crumbs = [{:name => t('home'), :link => '/'}]
         @bread_crumbs << {:name => t('archives.title'), :link => '/archives'}
         @bread_crumbs << {:name => year, :link => "/archives/#{year}"}
@@ -36,15 +34,6 @@ module Lokka
   end
 
   module Helpers
-    def posts_group_by_month(posts)
-      result = months.each_with_object({}) do |month, result|
-        result[month] = posts.select {|post|
-          post.created_at.strftime('%Y-%m') == "#{month.year}-#{month.month}"
-        }
-      end
-      result.delete_if {|key, value| value.blank? }
-    end
-
     def year_list
       first_year = Post.all.first.created_at.year
       last_year  = Post.all.last.created_at.year
