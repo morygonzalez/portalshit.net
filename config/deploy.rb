9824e16c0591bb73ed692bcc3999d4f5f6fdfced
森井ゴンザレス <morygonzalez@gmail.com>
2014-02-08 22:20:47 +0900

    configure newrelic.yml symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ set :deploy_to, "/home/morygonzalez/sites/deploys/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/newrelic.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
