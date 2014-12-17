c34d0828de9413826ce437e78fbb9bb978bee628
森井ゴンザレス <morygonzalez@gmail.com>
2014-12-17 23:17:51 +0900

    Plugin が評価されるのは起動時だけなのでヘルパーにして view から呼び出すようにした

--- public/plugin/lokka-twitter_card/lib/lokka/twitter_card.rb
+++ public/plugin/lokka-twitter_card/lib/lokka/twitter_card.rb
@@ -1,50 +1,46 @@
 module Lokka
   module TwitterCard
-    def self.registered(app)
-      app.before do
-        photo_identifier = /写真|photo|foto/
-        twitter_card = \
-          case
-          when defined?(@entry).nil?
-            <<-EOS
-              <meta name="twitter:card" content="summary">
-              <meta name="twitter:site" content="#{@site.title}">
-              <meta name="twitter:creator" content="@#{User.first.name}">
-              <meta name="twitter:title" content="#{@site.title}">
-              <meta name="twitter:description" content="#{@site.meta_description}" />
-              <meta name="twitter:image" content="#{detect_image_from()}" />
-              <meta name="twitter:url" content="#{@request.scheme}://#{@request.host}/" />
-            EOS
-          when @entry.category.title =~ photo_identifier
-            <<-EOS
-              <meta name="twitter:card" content="summary_large_image">
-              <meta name="twitter:site" content="#{@site.title}">
-              <meta name="twitter:creator" content="@#{@entry.user.name}">
-              <meta name="twitter:title" content="#{@entry.title}">
-              <meta name="twitter:description" content="#{extract_description_from(@entry)}" />
-              <meta name="twitter:image:src" content="#{detect_image_from(@entry)}">
-            EOS
-          else
-            <<-EOS
-              <meta name="twitter:card" content="summary" />
-              <meta name="twitter:site" content="#{@site.title}">
-              <meta name="twitter:creator" content="@#{@entry.user.name}">
-              <meta name="twitter:title" content="#{@entry.title}">
-              <meta name="twitter:description" content="#{extract_description_from(@entry)}" />
-              <meta name="twitter:image" content="#{detect_image_from(@entry)}" />
-              <meta name="twitter:url" content="#{@entry.link}" />
-            EOS
-          end
-        content_for :header do
-          twitter_card
-        end
-      end
-    end
+    def self.registered(app); end
   end
 
   module Helpers
+    def twitter_card(resource)
+      photo_identifier = /写真|photo|foto/
+      case
+      when !resource.instance_of?(Entry) && !resource.instance_of?(Post)
+        <<-EOS.strip_heredoc
+          <meta name="twitter:card" content="summary">
+          <meta name="twitter:site" content="#{@site.title}">
+          <meta name="twitter:creator" content="@#{User.first.name}">
+          <meta name="twitter:title" content="#{@site.title}">
+          <meta name="twitter:description" content="#{@site.meta_description}" />
+          <meta name="twitter:image" content="#{detect_image_from()}" />
+          <meta name="twitter:url" content="#{@request.scheme}://#{@request.host}/" />
+        EOS
+      when resource.category.title =~ photo_identifier
+        <<-EOS.strip_heredoc
+          <meta name="twitter:card" content="summary_large_image">
+          <meta name="twitter:site" content="#{@site.title}">
+          <meta name="twitter:creator" content="@#{resource.user.name}">
+          <meta name="twitter:title" content="#{resource.title}">
+          <meta name="twitter:description" content="#{extract_description_from(resource)}" />
+          <meta name="twitter:image:src" content="#{detect_image_from(resource)}">
+        EOS
+      else
+        <<-EOS.strip_heredoc
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="#{@site.title}">
+          <meta name="twitter:creator" content="@#{resource.user.name}">
+          <meta name="twitter:title" content="#{resource.title}">
+          <meta name="twitter:description" content="#{extract_description_from(resource)}" />
+          <meta name="twitter:image" content="#{detect_image_from(resource)}" />
+          <meta name="twitter:url" content="#{@request.scheme}://#{@request.host}/#{resource.link}" />
+        EOS
+      end
+    end
+
     def detect_image_from(resource=nil)
-      if resource.respond_to?(:body) && resource.body =~ /(https?\/\/(.+)\.(?:png|jpe?g|gif))/
+      if resource.respond_to?(:body) && resource.body =~ /(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+?\.(?:png|jpe?g|gif))/
         $1
       else
         "#{@request.scheme}://#{@request.host}#{@theme.path}/screenshot.png"
@@ -52,7 +48,7 @@ module Lokka
     end
 
     def extract_description_from(entry)
-      truncate(entry.body, 200)
+      truncate(strip_tags(entry.body).strip.gsub(/[\t]+/, ' ').gsub(/[\r\n]/, ''), length: 200)
     end
   end
 end
