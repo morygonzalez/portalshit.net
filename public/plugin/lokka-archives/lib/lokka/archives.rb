4e1c86d84a118adcdc0a738660a1f39bfe5b416e
森井ゴンザレス <morygonzalez@gmail.com>
2016-09-10 13:38:24 +0900

    Kill N+1 query

--- public/plugin/lokka-archives/lib/lokka/archives.rb
+++ public/plugin/lokka-archives/lib/lokka/archives.rb
@@ -3,57 +3,85 @@ require 'dm-serializer/to_json'
 module Lokka
   module Archives
     def self.registered(app)
-      app.set :cache_enabled, true
-
-      app.get '/archives' do
-        cache_path = app.cache_output_dir + '/archives.html'
-        if test(?f, cache_path) && Time.now - test(?M, cache_path) < 15.minutes
-          return File.read(cache_path)
-        end
-        @bread_crumbs = [{:name => t('home'), :link => '/'}]
-        @bread_crumbs << {:name => t('archives.title'), :link => '/archives'}
-        haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
+      app.before do
+        @categories = Category.all(fields: [:id, :slug, :title]).group_by(&:id)
       end
 
-      app.get '/api/archives' do
-        @month_posts = Post.all(draft: false, created_at: (1.year.ago..Time.now)).
-          group_by {|post| post.created_at.strftime('%Y-%1m') }
+      app.get '/archives.json' do
+        posts = Post.all(
+          fields: [:id, :category_id, :slug, :title, :created_at],
+          draft: false,
+          created_at: (1.year.ago..Time.now)
+        )
+
+        month_posts = posts.group_by {|post| post.created_at.strftime('%Y-%1m') }
+        month_posts = month_posts.each_with_object({}) {|(month, _posts), object|
+          object[month] ||= []
+          _posts.each do |post|
+            category = @categories[post.category_id].first
+            object[month] << {
+              id: post.id,
+              category: {
+                id: category[:id],
+                title: category[:title],
+                slug: category[:slug]
+              },
+              slug: post.slug,
+              title: post.title,
+              created_at: post.created_at
+            }
+          end
+          object
+        }
 
         content_type :json
-        fields_to_exclude = %i[body markup type slug user_id category_id updated_at draft frozen_tag_list]
-        methods_to_include = %i[link category]
-        @month_posts.to_json(
-          exclude: fields_to_exclude,
-          methods: methods_to_include
-        )
+        month_posts.to_json
       end
 
-      app.get '/archives/:year' do |year|
-        cache_path = app.cache_output_dir + "/archives/#{year}.html"
-        if test(?f, cache_path) && Time.now - test(?M, cache_path) < 15.minutes
-          return File.read(cache_path)
-        end
-        @bread_crumbs = [{:name => t('home'), :link => '/'}]
-        @bread_crumbs << {:name => t('archives.title'), :link => '/archives'}
-        @bread_crumbs << {:name => year, :link => "/archives/#{year}"}
-        haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
-      end
-
-      app.get '/api/archives/:year' do |year|
-        @month_posts = Post.all(
+      app.get '/archives/?:year?.json' do |year|
+        posts = Post.all(
+          fields: [:id, :category_id, :slug, :title, :created_at],
           :draft => false,
           :created_at => (
             Time.new(year)..Time.new(year).end_of_year
           )
-        ).group_by {|post| post.created_at.strftime('%Y-%1m') }
+        )
+
+        month_posts = posts.group_by {|post| post.created_at.strftime('%Y-%1m') }
+        month_posts = month_posts.each_with_object({}) {|(month, _posts), object|
+          object[month] ||= []
+          _posts.each do |post|
+            category = @categories[post.category_id].first
+            object[month] << {
+              id: post.id,
+              category: {
+                id: category[:id],
+                title: category[:title],
+                slug: category[:slug]
+              },
+              slug: post.slug,
+              title: post.title,
+              created_at: post.created_at
+            }
+          end
+          object
+        }
 
         content_type :json
-        fields_to_exclude = %i[body markup type slug user_id category_id updated_at draft frozen_tag_list]
-        methods_to_include = %i[link category]
-        @month_posts.to_json(
-          exclude: fields_to_exclude,
-          methods: methods_to_include
-        )
+        month_posts.to_json
+      end
+
+      app.get '/archives' do
+        @bread_crumbs = [{:name => t('home'), :link => '/'}]
+        @bread_crumbs << {:name => t('archives.title'), :link => '/archives'}
+        haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
+      end
+
+      app.get '/archives/:year' do |year|
+        @bread_crumbs = [{:name => t('home'), :link => '/'}]
+        @bread_crumbs << {:name => t('archives.title'), :link => '/archives'}
+        @bread_crumbs << {:name => year, :link => "/archives/#{year}"}
+        haml :"plugin/lokka-archives/views/index", :layout => :"theme/#{@theme.name}/layout"
       end
 
       app.before do
