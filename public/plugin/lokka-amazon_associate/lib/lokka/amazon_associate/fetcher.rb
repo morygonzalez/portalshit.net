f85aa1c3613c31c9817592b3087f3116312405c9
森井ゴンザレス <morygonzalez@gmail.com>
2016-11-04 22:39:49 +0900

    Wait 1 second before fetch content from Amazon Product Advertising API

--- public/plugin/lokka-amazon_associate/lib/lokka/amazon_associate/fetcher.rb
+++ public/plugin/lokka-amazon_associate/lib/lokka/amazon_associate/fetcher.rb
@@ -31,12 +31,11 @@ module Lokka
       end
 
       def wirite_or_touch_cache
-        if item = fetch
-          File.open(@path, "w") {|f|
-            f.print Hash.from_xml(item.doc.to_xml).to_json
-            sleep 2
-          }
-        else
+        begin
+          sleep 1
+          item = fetch
+          File.open(@path, "w") {|f| f.print Hash.from_xml(item.doc.to_xml).to_json }
+        rescue
           FileUtils.touch @path
         end
       end
