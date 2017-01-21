e94cf86f8b158603f47b446070893d158deec166
森井ゴンザレス <morygonzalez@gmail.com>
2017-01-21 14:58:30 +0900

    Use config/puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,8 @@ set :linked_dirs, %w{log tmp/pids tmp/sockets tmp/amazon vendor/bundle}
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :puma_conf, File.join(current_path, 'config', 'puma.rb')
+
 set :db_host, 'localhost'
 set :bundle_without, %w{development test postgresql sqlite}.join(' ')
 
