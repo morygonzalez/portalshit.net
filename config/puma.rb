097562ebe6378bb8a25b3ece87cb24950bf49d0e
森井ゴンザレス <morygonzalez@gmail.com>
2017-01-22 04:23:42 +0900

    Configure puma single process mode

--- config/puma.rb
+++ config/puma.rb
@@ -6,26 +6,26 @@ environment 'production'
 
 pidfile "/home/morygonzalez/sites/deploys/portalshit/shared/tmp/pids/puma.pid"
 state_path "/home/morygonzalez/sites/deploys/portalshit/shared/tmp/pids/puma.state"
-stdout_redirect '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_access.log', '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_error.log', true
+stdout_redirect '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_stdout.log', '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_stderr.log', true
 
 threads 0,16
 
 bind 'unix:///home/morygonzalez/sites/deploys/portalshit/shared/tmp/sockets/puma.sock'
 
-workers 2
+workers 0
+prune_bundler
 
-# prune_bundler
-
-before_fork do
-  require 'puma_worker_killer'
-
-  PumaWorkerKiller.config do |config|
-    config.ram           = 512 # mb
-    config.frequency     = 5   # seconds
-    config.percent_usage = 0.80
-    config.enable_rolling_restart
-  end
-end
+# workers 2
+# before_fork do
+#   require 'puma_worker_killer'
+#
+#   PumaWorkerKiller.config do |config|
+#     config.ram           = 300 # mb
+#     config.frequency     = 5   # seconds
+#     config.percent_usage = 0.70
+#     config.enable_rolling_restart
+#   end
+# end
 
 on_restart do
   puts 'Refreshing Gemfile'
