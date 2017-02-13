136e3f1c9d55df13c7531d54b7f86b145382d895
森井ゴンザレス <morygonzalez@gmail.com>
2017-02-13 22:03:47 +0900

    Use puma_worker_killer again

--- config/puma.rb
+++ config/puma.rb
@@ -8,24 +8,25 @@ pidfile "/home/morygonzalez/sites/deploys/portalshit/shared/tmp/pids/puma.pid"
 state_path "/home/morygonzalez/sites/deploys/portalshit/shared/tmp/pids/puma.state"
 stdout_redirect '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_stdout.log', '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_stderr.log', true
 
-threads 0,16
+threads 0,8
 
 bind 'unix:///home/morygonzalez/sites/deploys/portalshit/shared/tmp/sockets/puma.sock'
 
 workers 0
-prune_bundler
+# prune_bundler
+preload_app!
 
-# workers 2
-# before_fork do
-#   require 'puma_worker_killer'
-#
-#   PumaWorkerKiller.config do |config|
-#     config.ram           = 300 # mb
-#     config.frequency     = 5   # seconds
-#     config.percent_usage = 0.70
-#     config.enable_rolling_restart
-#   end
-# end
+workers 2
+before_fork do
+  require 'puma_worker_killer'
+
+  PumaWorkerKiller.config do |config|
+    config.ram           = 300 # mb
+    config.frequency     = 5   # seconds
+    config.percent_usage = 0.70
+    config.enable_rolling_restart
+  end
+end
 
 on_restart do
   puts 'Refreshing Gemfile'
