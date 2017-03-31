c49722518e6d5b90ba10061d21fee6a0b02390d6
森井ゴンザレス <morygonzalez@gmail.com>
2017-04-01 07:10:32 +0900

    Use same puma.rb on production and development

--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,43 @@
 #!/usr/bin/env puma
 
-directory '/home/morygonzalez/sites/deploys/portalshit/current'
-rackup "/home/morygonzalez/sites/deploys/portalshit/current/config.ru"
-environment 'production'
+directory File.expand_path(File.join(File.dirname(__FILE__), '../'))
+rackup 'config.ru'
 
-pidfile "/home/morygonzalez/sites/deploys/portalshit/shared/tmp/pids/puma.pid"
-state_path "/home/morygonzalez/sites/deploys/portalshit/shared/tmp/pids/puma.state"
-stdout_redirect '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_stdout.log', '/home/morygonzalez/sites/deploys/portalshit/shared/log/puma_stderr.log', true
+environment ENV['RACK_ENV']
+
+if ENV['RACK_ENV'] == 'production'
+  tmp_dir = File.join(get(:directory),'tmp')
+  log_dir = File.join(get(:directory), 'log')
+  pid_dir = File.join(tmp_dir, 'pids')
+
+  pidfile         File.join(pid_dir, 'puma.pid')
+  state_path      File.join(pid_dir, 'puma.state')
+  stdout_redirect File.join(log_dir, 'puma_stdout.log'), File.join(log_dir, 'puma_stderr.log'), true
+  bind            "unix://#{File.join(tmp_dir, 'sockets', 'puma.sock')}"
+
+  preload_app!
+
+  workers 2
+  before_fork do
+    require 'puma_worker_killer'
+
+    PumaWorkerKiller.config do |config|
+      config.ram           = 300 # mb
+      config.frequency     = 5   # seconds
+      config.percent_usage = 0.70
+      config.enable_rolling_restart
+    end
+  end
+
+  on_restart do
+    puts 'Refreshing Gemfile'
+    ENV["BUNDLE_GEMFILE"] = "/home/morygonzalez/sites/deploys/portalshit/current/Gemfile"
+  end
+
+else
+  stdout_redirect 'log/puma_stdout.log', 'log/puma_stderr.log', true
+  workers 0
+  prune_bundler
+end
 
 threads 0,8
-
-bind 'unix:///home/morygonzalez/sites/deploys/portalshit/shared/tmp/sockets/puma.sock'
-
-workers 0
-# prune_bundler
-preload_app!
-
-workers 2
-before_fork do
-  require 'puma_worker_killer'
-
-  PumaWorkerKiller.config do |config|
-    config.ram           = 300 # mb
-    config.frequency     = 5   # seconds
-    config.percent_usage = 0.70
-    config.enable_rolling_restart
-  end
-end
-
-on_restart do
-  puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = "/home/morygonzalez/sites/deploys/portalshit/current/Gemfile"
-end
