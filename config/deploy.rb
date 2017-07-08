bc504cce885dba4417937ae6680c26b4747ed5f2
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2017-07-08 14:56:36 +0900

    Use locally installed rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.7.1'
 
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} /usr/bin/rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} rbenv exec"
 set :default_env, { 'RUBYOPT' => '--encoding=UTF-8' }
 
 set :application, 'portalshit'
