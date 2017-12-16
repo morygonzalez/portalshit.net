91507b31cf7457abba76acf9067ea3f5d991e128
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2017-12-16 16:25:14 +0900

    Refine popular entries' SQL

--- public/plugin/lokka-popular_entries/lib/lokka/popular_entries.rb
+++ public/plugin/lokka-popular_entries/lib/lokka/popular_entries.rb
@@ -8,14 +8,14 @@ class Entry
   class << self
     def popular(count = 5)
       access_ranking = File.open(File.join(Lokka.root, 'public', 'access-ranking.txt'))
-      result = []
+      slugs = {}
       access_ranking.each.with_index(1) do |line, index|
-        _, path = *line.split(" ")
-        path = path.split("/")[-1]
-        result << Entry.get_by_fuzzy_slug(path)
+        access_count, path = *line.split(" ")
+        slug = path.split("/")[-1]
+        slugs[access_count] = slug
         break if index == count
       end
-      result
+      all(slug: slugs.values, limit: count).sort_by {|entry| slugs.values.index(entry.slug) }
     end
   end
 end
