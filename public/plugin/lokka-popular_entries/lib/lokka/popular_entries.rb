1be3666a7363907468c1b857220bade838348154
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-02-07 18:34:14 +0900

    Show hotentry

--- public/plugin/lokka-popular_entries/lib/lokka/popular_entries.rb
+++ public/plugin/lokka-popular_entries/lib/lokka/popular_entries.rb
@@ -5,17 +5,41 @@ module Lokka
 end
 
 class Entry
+  attr_accessor :bookmark_count
+
   class << self
-    def popular(count = 5)
+    def popular(limit = 5)
       access_ranking = File.open(File.join(Lokka.root, 'public', 'access-ranking.txt'))
       slugs = {}
       access_ranking.each.with_index(1) do |line, index|
-        access_count, path = *line.split(" ")
+        access_limit, path = *line.split(" ")
         slug = path.split("/")[-1]
-        slugs[access_count] = slug
-        break if index == count
+        slugs[access_limit] = slug
+        break if index == limit
       end
-      all(slug: slugs.values, limit: count).sort_by {|entry| slugs.values.index(entry.slug) }
+      all(slug: slugs.values, limit: limit).sort_by {|entry| slugs.values.index(entry.slug) }
+    rescue
+      []
+    end
+
+    def hotentry(limit = 5)
+      require 'open-uri'
+      require 'nokogiri'
+
+      max = limit - 1
+      url = 'http://b.hatena.ne.jp/entrylist?sort=count&url=portalshit.net'
+      ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+      content = open(url, 'User-Agent' => ua).read
+      doc = Nokogiri::HTML(content)
+      slugs = doc.xpath('//li[contains(@class, "entry-unit")]')[0..max].inject({}) {|result, item|
+        attributes = item.xpath('div[2]/h3/a')
+        slug = attributes.attr('href').value.gsub(%r{https?://(www\.)?portalshit\.net/(\d{4}/\d{2}/\d{2}/|article\.php\?id=)}, '')
+        count = item.attr('data-bookmark-count')
+        result[slug] = count
+        result
+      }
+      entries = all(slug: slugs.keys, limit: limit).sort_by {|entry| slugs.keys.index(entry.slug) }
+      entries.map {|entry| entry.bookmark_count = slugs[entry.slug]; entry }
     rescue
       []
     end
