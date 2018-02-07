3082a65c50693dc677942f0e0da49731f323865e
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-02-07 19:24:29 +0900

    Link to bookmark url

--- public/plugin/lokka-popular_entries/lib/lokka/popular_entries.rb
+++ public/plugin/lokka-popular_entries/lib/lokka/popular_entries.rb
@@ -5,7 +5,7 @@ module Lokka
 end
 
 class Entry
-  attr_accessor :bookmark_count
+  attr_accessor :bookmark_count, :bookmark_url
 
   class << self
     def popular(limit = 5)
@@ -33,15 +33,22 @@ class Entry
       doc = Nokogiri::HTML(content)
       slugs = doc.xpath('//li[contains(@class, "entry-unit")]')[0..max].inject({}) {|result, item|
         attributes = item.xpath('div[2]/h3/a')
+        meta = item.xpath('div[2]/ul/li/span/a')[0]
+        entry_url = '/entry/' + attributes.attr('href').value.gsub(%r{https?://}, '')
         slug = attributes.attr('href').value.gsub(%r{https?://(www\.)?portalshit\.net/(\d{4}/\d{2}/\d{2}/|article\.php\?id=)}, '')
-        count = item.attr('data-bookmark-count')
-        result[slug] = count
+        bookmark_count = item.attr('data-bookmark-count')
+        bookmark_url = "http://b.hatena.ne.jp#{meta&.attr('href') || entry_url}"
+        result[slug] = { bookmark_count: bookmark_count, bookmark_url: bookmark_url }
         result
       }
       entries = all(slug: slugs.keys, limit: limit).sort_by {|entry| slugs.keys.index(entry.slug) }
-      entries.map {|entry| entry.bookmark_count = slugs[entry.slug]; entry }
-    rescue
-      []
+      entries.map {|entry|
+        entry.bookmark_count = slugs[entry.slug][:bookmark_count]
+        entry.bookmark_url = slugs[entry.slug][:bookmark_url]
+        entry
+      }
+    # rescue
+    #   []
     end
   end
 end
