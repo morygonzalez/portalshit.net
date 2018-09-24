edfb76f8c9d40129f5e58e574bb76220535c5b71
森井ゴンザレス <morygonzalez@gmail.com>
2018-09-24 09:31:52 +0900

    Only show published references

--- public/plugin/lokka-referrerable/lib/lokka/referrerable.rb
+++ public/plugin/lokka-referrerable/lib/lokka/referrerable.rb
@@ -7,7 +7,7 @@ module Lokka
 
   module Helpers
     def referrers
-      Entry.all(:body.like => "%#{@entry.link}%")
+      Entry.published.all(:body.like => "%#{@entry.link}%")
     end
   end
 end
