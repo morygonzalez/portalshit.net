033a15596672ac4d6454b84ddd39a4aed54719ae
森井ゴンザレス <morygonzalez@gmail.com>
2019-08-19 22:56:11 +0900

    Ajax markdown preview

--- public/admin/js/src/FormObserver.js
+++ public/admin/js/src/FormObserver.js
@@ -7,6 +7,7 @@ class FormObserver {
     this.selected = document.querySelector('select[id$=_markup] option:checked').value;
     this.setupEditor();
     this.observeSubmit();
+    this.observePreview();
   }
 
   setupEditor() {
@@ -27,6 +28,8 @@ class FormObserver {
 
   setupQuill() {
     const content = document.querySelector('#editor textarea').value;
+    const previewTab = document.querySelector('ul.preview-edit');
+    previewTab.style.display = 'none';
     this.quill = new Quill('#editor', {
       modules: {
         toolbar:[
@@ -46,9 +49,10 @@ class FormObserver {
 
   setupTextarea() {
     const textarea = this.textarea;
+    const previewTab = document.querySelector('ul.preview-edit');
+    previewTab.style.display = 'block';
     this.quill = Quill.find(document.querySelector('#editor'));
     if (this.quill) {
-      const label = document.querySelector('label[for$="_body"]');
       const qlToolbar = document.querySelector('.ql-toolbar');
       const qlContainer = document.querySelector('.ql-container');
       const content = this.quill.root.innerHTML;
@@ -58,7 +62,7 @@ class FormObserver {
       const editor = document.createElement('div');
       editor.setAttribute('id', 'editor');
       editor.appendChild(textarea);
-      label.parentNode.insertBefore(editor, label.nextSibling);
+      previewTab.parentNode.insertBefore(editor, previewTab.nextSibling);
     }
   }
 
@@ -90,6 +94,56 @@ class FormObserver {
     form.onsubmit = () => {
       this.setupTextarea();
     }
+  }
+
+  observePreview() {
+    let editor;
+    const preview = document.querySelector('#preview');
+    const previewRadio = document.querySelector('input[type="radio"][name="preview"][value="preview"]');
+    const editRadio = document.querySelector('input[type="radio"][name="preview"][value="edit"]');
+    editRadio.addEventListener('change', (e) => {
+      editor = document.querySelector('#editor');
+      e.srcElement.parentElement.classList.add('selected');
+      previewRadio.parentElement.classList.remove('selected');
+      preview.style.display = 'none';
+      editor.style.display = 'block';
+    });
+    previewRadio.addEventListener('change', (e) => {
+      editor = document.querySelector('#editor');
+      e.srcElement.parentElement.classList.add('selected');
+      editRadio.parentElement.classList.remove('selected');
+      const textarea = document.querySelector('#editor textarea');
+      const body = textarea.value;
+      const markup = document.querySelector('#post_markup option[selected="selected"]').value;
+      const self = this;
+      const ajaxData = new FormData();
+      ajaxData.append('raw_body', body);
+      ajaxData.append('markup', markup);
+      let promise = new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        let response;
+        xhr.open('POST', '/admin/previews');
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState != 4) {
+            // waiting response
+          } else if (xhr.status != 201) {
+            response = JSON.parse(xhr.response);
+            reject(response);
+          } else {
+            response = JSON.parse(xhr.response);
+            resolve(response);
+          }
+        }
+        xhr.send(ajaxData);
+      });
+      promise.then(response => {
+        preview.innerHTML = response.body;
+        editor.style.display = 'none';
+        preview.style.display = 'block';
+      }).catch(response => {
+        console.error(response.message);
+      });
+    });
   }
 }
 
