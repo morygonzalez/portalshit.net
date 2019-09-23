08d9ca17570390cd8e217126c33c215eb2315311
森井ゴンザレス <morygonzalez@gmail.com>
2019-09-23 10:30:17 +0900

    Fix ogp fetcher bug that replace image link

--- public/plugin/lokka-ogp/lib/lokka/ogp/fetcher.rb
+++ public/plugin/lokka-ogp/lib/lokka/ogp/fetcher.rb
@@ -18,7 +18,8 @@ module Lokka
       def fetch
         doc.xpath('./p').each do |node|
           next if node.children.length > 1
-          next unless node.inner_html =~ %r|\A<a href.+?/a>\Z|
+          next if node.inner_html =~ %r|img src|
+          next if node.inner_html !~ %r|\A<a href.+?/a>\Z|
           url = node.xpath('./a').first.attributes["href"].value
           escaped_url = CGI.escape(url)
           each_fetcher = EachFetcher.new(url)
