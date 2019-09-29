7b31c7c5f9b23a529dd2b7660f3a249343b4349f
森井ゴンザレス <morygonzalez@gmail.com>
2019-09-29 21:55:54 +0900

    Stop using JavaScript to detect random background image

--- public/theme/portalshit/scripts/index.js
+++ public/theme/portalshit/scripts/index.js
@@ -16,28 +16,3 @@ $(function() {
     });
   }
 });
-
-(() => {
-  const images = [
-    'black-kite',
-    'container-ship',
-    'daytime-aso',
-    'evening-aso',
-    'evening-moon',
-    'fire',
-    'halo',
-    'heimin',
-    'jizou',
-    'karatsu',
-    'momiji',
-    'moss',
-    'nagatare',
-    'nanzenji',
-    'night-moon',
-    'wave',
-  ];
-
-  const image = images[Math.floor(Math.random() * images.length)];
-  const imageUrl = `https://resources.portalshit.net/theme-images/header-bg-${image}.jpg`;
-  document.querySelector('#header').style.backgroundImage = `linear-gradient(to top, rgba(33,33,33,0.80), transparent, transparent), url("${imageUrl}")`;
-})();
