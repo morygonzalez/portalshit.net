fd69bd5958c7fa199db39b71483675b6dd3b3227
森井ゴンザレス <morygonzalez@gmail.com>
2019-12-16 07:07:12 +0900

    Update iframe style

--- public/plugin/lokka-amazon_associate/lib/lokka/amazon_associate/html_formatter.rb
+++ public/plugin/lokka-amazon_associate/lib/lokka/amazon_associate/html_formatter.rb
@@ -5,6 +5,8 @@ module Lokka
     class HtmlFormatter
       include CacheControllable
 
+      attr_reader :item_id
+
       def initialize(item_id)
         @item_id = item_id
         @kind = :html
@@ -17,26 +19,78 @@ module Lokka
 
       def format
         template = <<~ERUBY
-          <div class="amazon">
-            <div class="amazon-image">
-              <a href="<%= item.link %>}"><img src="<%= item.image %>" /></a>
-            </div>
-            <div class="amazon-content">
-              <ul>
-                <li><a href="<%= item.link %>"><%= item.title %></a></li>
-                <% if item.manufacturer.present? %>
-                <li><%= item.manufacturer %></li>
-                <% end %>
-                <% if item.author.present? %>
-                <li><%= item.author %></li>
-                <% end %>
-                <% if item.binding.present? %>
-                <li><%= item.binding %></li>
-                <% end %>
-                <li><a href="<%= item.link %>"><%= item.price %></a></li>
-              </ul>
-            </div>
-          </div>
+          <!DOCTYPE html>
+          <html lang="ja">
+            <head>
+              <title><%= item.title %></title>
+              <style>
+                .amazon {
+                  display: flex;
+                  margin: 1em auto;
+                  max-height: 320px;
+                  background: #fff;
+                  border-radius: 4px;
+                  color: #000;
+                }
+                .amazon a {
+                  text-shadow: none;
+                }
+                .amazon-image {
+                  flex-grow: 1;
+                  width: 300px;
+                  margin: 1em auto;
+                  display: flex;
+                  justify-content: center;
+                }
+                .amazon-image img {
+                  border-radius: 4px;
+                  max-height: 280px;
+                  max-width: 280px;
+                }
+                .amazon-content {
+                  margin: 1em auto;
+                  flex-grow: 4;
+                }
+                .amazon-content ul {
+                  margin-top: 0;
+                  list-style: disc;
+                }
+                @media screen and (max-width:640px) {
+                  .amazon-content, .amazon-image {
+                    float: none;
+                    max-width: 50%;
+                  }
+                  .amazon-image img {
+                    border-radius: 4px;
+                    max-height: 150px;
+                    max-width: 150px;
+                  }
+                }
+              </style>
+            </head>
+            <body>
+              <div class="amazon">
+                <div class="amazon-image">
+                  <a href="<%= item.link %>}"><img src="<%= item.image %>" /></a>
+                </div>
+                <div class="amazon-content">
+                  <ul>
+                    <li><a href="<%= item.link %>"><%= item.title %></a></li>
+                    <% if item.manufacturer.present? %>
+                    <li><%= item.manufacturer %></li>
+                    <% end %>
+                    <% if item.author.present? %>
+                    <li><%= item.author %></li>
+                    <% end %>
+                    <% if item.binding.present? %>
+                    <li><%= item.binding %></li>
+                    <% end %>
+                    <li><a href="<%= item.link %>"><%= item.price %></a></li>
+                  </ul>
+                </div>
+              </div>
+            </body>
+          </html>
         ERUBY
         erb = ERB.new(template)
         erb.result(binding)
