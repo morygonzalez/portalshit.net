1cf242548633d19188e92c97aa9d51aa67ca469c
Hitoshi Nakashima <morygonzalez@gmail.com>
2019-12-16 11:42:30 +0900

    Update expander.rb

--- public/plugin/lokka-amazon_associate/lib/lokka/amazon_associate/expander.rb
+++ public/plugin/lokka-amazon_associate/lib/lokka/amazon_associate/expander.rb
@@ -16,7 +16,13 @@ module Lokka
         matches.each do |item_id|
           html = HtmlFormatter.new(item_id)
           base_url = "https://portalshit.net"
-          iframe = %Q(<iframe src="#{base_url}/amazon/#{item_id}.html" />)
+          iframe = <<~HTML
+            <iframe
+               src="#{base_url}/amazon/#{item_id}.html"
+               title="#{html.item.title}" scrolling="no" frameborder="0"
+               style="display: block; width: 100%; height: 320px; max-width: 500px; margin: 10px 0px;">
+            </iframe>
+          HTML
           body.gsub!(/<!--\s(?:ASIN|ISBN)=#{html.item_id}\s-->/, iframe)
         end
 
