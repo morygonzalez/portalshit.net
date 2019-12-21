606763b866ab77f10d9af427ae4e04f62c0dc728
森井ゴンザレス <morygonzalez@gmail.com>
2019-12-21 19:15:36 +0900

    Ranking Today

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,17 @@ set :deploy_to, "/home/morygonzalez/sites/deploys/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
-set :linked_files, %w{config/newrelic.yml database.yml .env public/access-ranking.txt public/referer-ranking.txt public/ads.txt}
+set :linked_files,
+  %w{
+    config/newrelic.yml
+    database.yml
+    .env
+    public/access-ranking.txt
+    public/referer-ranking.txt
+    public/access-ranking-today.txt
+    public/referer-ranking-today.txt
+    public/ads.txt
+  }
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
