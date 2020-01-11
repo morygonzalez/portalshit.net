b6dfa9b060ffc89e479c16984a50c8d06f810b57
森井ゴンザレス <morygonzalez@gmail.com>
2020-01-11 11:33:14 +0900

    Deferred image loading

--- public/theme/portalshit/scripts/index.js
+++ public/theme/portalshit/scripts/index.js
@@ -1,13 +1,41 @@
-document.body.addEventListener(
-  "AutoPagerize_DOMNodeInserted",
-  function(e) {
+document.body.addEventListener("AutoPagerize_DOMNodeInserted", function(e) {
     let node       = e.target
     let requestURL = e.newValue
     let parentNode = e.relatedNode
     init(node);
-  },
-  false
+  }, false
 );
+
+document.addEventListener("DOMContentLoaded", function() {
+  let selectors = "#content article .body img, #content article .similar img";
+  var lazyImages = [].slice.call(document.querySelectorAll(selectors));
+
+  for (let image of lazyImages) {
+    let src = image.src;
+    image.dataset.src = src;
+    image.src = "";
+  };
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          // lazyImage.srcset = lazyImage.dataset.srcset;
+          // lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to a more compatible method here
+  }
+});
 
 $(function() {
   if ($(window).width() <= 640) {
