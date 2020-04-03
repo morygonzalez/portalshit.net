39b5d4f1b01e786fa89c822c194e86c8a0a949c9
森井ゴンザレス <morygonzalez@gmail.com>
2020-04-04 03:00:47 +0900

    Fix amazon associate tag expansion

--- public/plugin/lokka-amazon_associate/assets/script.js
+++ public/plugin/lokka-amazon_associate/assets/script.js
@@ -1,53 +1,32 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const regexp = /(?:ISBN|ASIN)=([0-9A-Z]+)/g;
   const COMMENT_NODE = 8;
-  let bodies = document.querySelectorAll('div.body');
-  bodies.forEach(function(body) {
-    body.childNodes.forEach(function(node) {
+  const bodies = document.querySelectorAll('article > .body');
+  bodies.forEach(body => {
+    body.childNodes.forEach(async node => {
       if (node.nodeType !== COMMENT_NODE)
         return;
       if (!node.textContent.match(regexp))
         return;
-      let itemId = RegExp.$1;
-      let url = `/amazon/${itemId}.html`;
-      let request = new XMLHttpRequest();
-      request.responseType = 'html';
-      let promise = new Promise(function(resolve, reject) {
-        request.open('GET', url);
-        request.onreadystatechange = function() {
-          if (request.readyState != 4) {
-            // リクエスト中
-          } else if (request.status != 200) {
-            // 失敗
-            reject(request.response);
-          } else {
-            // 取得成功
-            let result = request.response;
-            resolve(result);
+      const itemId = RegExp.$1;
+      const url = `/amazon/${itemId}.html`;
+      const request = await fetch(url);
+      const response = await request.text();
+      const previous = node.previousSibling;
+      const parent = node.parentNode;
+      const d = document.createElement('div');
+      d.innerHTML = response;
+      d.querySelectorAll('a').forEach(item => {
+        item.onclick = (e) => {
+          const productTitle = item.title;
+          if (typeof ga !== 'undefined') {
+            ga('send', 'event', 'Amazon Affiliate', 'click', productTitle);
           }
         };
-        request.send();
       });
-      promise.then(function(result) {
-        let previous = node.previousSibling;
-        let parent = node.parentNode;
-        let d = document.createElement('div');
-        d.innerHTML = result;
-        d.querySelectorAll('a').forEach(function(item) {
-          item.onclick = function(e) {
-            let productTitle = item.textContent || item.title;
-            if (typeof ga !== 'undefined') {
-              ga('send', 'event', 'Amazon Affiliate', 'click', productTitle);
-            }
-          };
-        });
-        parent.insertBefore(d, previous.nextSibling);
-      }).catch(function(error) {
-        console.log(error);
-      });
-      return promise;
+      parent.insertBefore(d, previous.nextSibling);
     });
   });
 });
