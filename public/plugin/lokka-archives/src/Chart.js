2c80ce880042313cc3e5824730319180cf91a0f2
森井ゴンザレス <morygonzalez@gmail.com>
2020-04-18 09:14:12 +0900

    Style Update 2020-04-18
    - Font Awesome version bump
    - Navigation style improvement, text ellipsis
    - Similar/Referer Component title ellipsis

--- public/plugin/lokka-archives/src/Chart.js
+++ public/plugin/lokka-archives/src/Chart.js
@@ -14,7 +14,7 @@ export default class Chart extends PureComponent {
     }
     this.colors = [
       '#E53935', '#D81B60', '#8E24AA', '#5E35B1', '#3949AB', '#1E88E5', '#039BE5',
-      '#00ACC1', '#00897B', '#43A047', '#7CB342', '#C0CA33', '#FDD835'
+      '#00ACC1', '#00897B', '#43A047', '#7CB342', '#C0CA33', '#FDD835', '#FFB300'
     ]
     this.selectBar = this.selectBar.bind(this)
   }
