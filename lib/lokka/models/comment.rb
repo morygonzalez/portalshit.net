fdccd009e9cbda8ce1f84211d6e6296b6df5c46a
Hitoshi Nakashima <morygonzalez@gmail.com>
2020-05-10 17:50:09 +0900

    Move comment notification code from model to patches

--- lib/lokka/models/comment.rb
+++ lib/lokka/models/comment.rb
@@ -11,8 +11,6 @@ class Comment < ActiveRecord::Base
   validates :body, presence: true
   validates_length_of :email, in: (0..40), if: ->(record) { record.email.present? }
 
-  after_save :send_notification_to_entry_author
-
   default_scope -> { order('created_at DESC') }
 
   scope :moderated, -> { where(status: MODERATED) }
@@ -23,49 +21,5 @@ class Comment < ActiveRecord::Base
 
   def link
     (entry ? "#{entry.link}#comment-#{id}" : '#')
-  end
-
-  private
-
-  def send_notification_to_entry_author
-    return if Lokka.test?
-    return if email == entry.user.email
-    credentials = Aws::Credentials.new(Option.aws_access_key_id, Option.aws_secret_access_key)
-    region = 'us-east-1'
-    client = Aws::SESV2::Client.new(credentials: credentials, region: region)
-    client.send_email(notification_params)
-  end
-
-  def notification_params
-    from = 'portal shit! <info@portalshit.net>'
-    to = entry.user.email
-    subject_data = %Q(#{name} commented on your entry "#{entry.title}")
-    subject_data = "[#{Lokka.env}] #{subject_data}" unless Lokka.production?
-    body_data = <<~TEXT
-      You have received comment from #{name} on "#{entry.title}", at #{created_at}
-
-      #{body.lines.map {|line| "> #{line}" }.join("\n")}
-
-      See full conversation https://portalshit.net#{link}
-    TEXT
-    {
-      from_email_address: from,
-      destination: { to_addresses: [to] },
-      content: {
-        simple: {
-          subject: {
-            data: subject_data
-          },
-          body: {
-            text: {
-              data: body_data
-            },
-            html: {
-              data: Markup.use_engine('redcarpet', body_data)
-            }
-          }
-        }
-      }
-    }
   end
 end
--- public/plugin/lokka-portalshit_patches/lib/lokka/portalshit_patches.rb
+++ public/plugin/lokka-portalshit_patches/lib/lokka/portalshit_patches.rb
@@ -48,3 +48,53 @@ class Entry
     images.first || 'https://portalshit.net/theme/portalshit/ogp_image.png'
   end
 end
+
+class Comment
+  after_save :send_notification_to_entry_author
+
+  private
+
+  def send_notification_to_entry_author
+    return if Lokka.test?
+    return if email == entry.user.email
+    return if status == SPAM
+
+    credentials = Aws::Credentials.new(Option.aws_access_key_id, Option.aws_secret_access_key)
+    region = 'us-east-1'
+    client = Aws::SESV2::Client.new(credentials: credentials, region: region)
+    client.send_email(notification_params)
+  end
+
+  def notification_params
+    from = 'portal shit! <info@portalshit.net>'
+    to = entry.user.email
+    subject_data = %Q(#{name} commented on your entry "#{entry.title}")
+    subject_data = "[#{Lokka.env}] #{subject_data}" unless Lokka.production?
+    body_data = <<~TEXT
+      You have received comment from #{name} on "#{entry.title}", at #{created_at}
+
+      #{body.lines.map {|line| "> #{line}" }.join("\n")}
+
+      See full conversation https://portalshit.net#{link}
+    TEXT
+    {
+      from_email_address: from,
+      destination: { to_addresses: [to] },
+      content: {
+        simple: {
+          subject: {
+            data: subject_data
+          },
+          body: {
+            text: {
+              data: body_data
+            },
+            html: {
+              data: Markup.use_engine('redcarpet', body_data)
+            }
+          }
+        }
+      }
+    }
+  end
+end
