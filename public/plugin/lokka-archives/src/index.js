da55ade7287f277b0964a724253b5f0d49dc3d10
Hitoshi Nakashima <morygonzalez@gmail.com>
2022-05-25 18:45:54 +0900

    Pin react version @17

--- public/plugin/lokka-archives/src/index.js
+++ public/plugin/lokka-archives/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render( <App />)
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
