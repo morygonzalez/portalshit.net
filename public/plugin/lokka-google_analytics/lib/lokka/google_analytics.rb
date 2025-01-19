a491d1eab4edd9cf6f49d05a74d6d017ca3dba74
Hitoshi Nakashima <morygonzalez@gmail.com>
2025-01-19 19:25:51 +0900

    Remove UA tracking code

--- public/plugin/lokka-google_analytics/lib/lokka/google_analytics.rb
+++ public/plugin/lokka-google_analytics/lib/lokka/google_analytics.rb
@@ -18,11 +18,6 @@ module Lokka
       app.before do
         tracker = Option.tracker
         tracker_ga4 = Option.tracker_ga4
-        if tracker.present? && Lokka.production? && !logged_in?
-          content_for :header do
-            ua_tracking_code
-          end
-        end
         if tracker_ga4.present? && Lokka.production? && !logged_in?
           content_for :header do
             ga4_tracking_code
@@ -33,20 +28,6 @@ module Lokka
   end
 
   module Helpers
-    def ua_tracking_code
-      <<-JAVASCRIPT.strip_heredoc.html_safe
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', '#{Option.tracker}', 'auto');
-          ga('send', 'pageview');
-        </script>
-      JAVASCRIPT
-    end
-
     def ga4_tracking_code
       <<-JAVASCRIPT.strip_heredoc.html_safe
         <!-- Global site tag (gtag.js) - Google Analytics -->
