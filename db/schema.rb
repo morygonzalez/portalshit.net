df864ed67d9ff70f6e3f806ab1b3fe3a57d8f8f7
Hitoshi Nakashima <morygonzalez@gmail.com>
2026-02-28 20:32:05 +0900

    Run db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_02_25_000000) do
+ActiveRecord::Schema.define(version: 2026_02_28_000000) do
 
   create_table "activities", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id", null: false
